822df3c8f4eea6259ffeb848e3c8da69a356d302
Chris Neal <chrisvneal@gmail.com>
2021-12-16 19:03:39 -1000

    Initial commit

---
+++ index.js
@@ -0,0 +1,117 @@
+// Create button to start game
+const playGameBtn = document.querySelector("#playGameBtn");
+playGameBtn.addEventListener("click", startGame);
+
+// create the answers
+const answers = ["rock", "paper", "scissors"];
+
+// returns randomly selected answer for computer (util)
+function computerPlay() {
+  let randNum = Math.floor(Math.random() * 3);
+  return answers[randNum];
+}
+
+// deduct a single point from player score (util)
+function deductPoint(player) {
+  player == 0 ? (player = 0) : player--;
+}
+
+// display total scores after game (console)
+function displayScore(playerScore, computerScore) {
+  playerScore > computerScore
+    ? console.log("Player wins!")
+    : console.log("Computer wins!");
+  console.log("Player score: ", playerScore);
+  console.log("Computer score: ", computerScore);
+}
+
+// keep players score
+let playerScore = 0;
+let computerScore = 0;
+
+// Return results from a single round
+const playRound = (playerSelection, computerSelection) => {
+  switch (playerSelection) {
+    case "rock":
+      switch (computerSelection) {
+        case "rock":
+          return "Tie! Go again!!";
+          break;
+        case "paper":
+          deductPoint(playerScore);
+          computerScore++;
+          return "You lose! Paper crushes rock";
+          break;
+        case "scissors":
+          deductPoint(computerScore);
+          playerScore++;
+          return "You win! Rock crushes scissors";
+          break;
+        default:
+          return;
+      }
+    case "paper":
+      switch (computerSelection) {
+        case "paper":
+          return "Tie! Go again!!";
+          break;
+        case "rock":
+          deductPoint(computerScore);
+          playerScore++;
+          return "You win! Paper covers rock!!";
+          break;
+        case "scissors":
+          deductPoint(playerScore);
+          computerScore++;
+          return "You lose! Scissors cut paper!";
+          break;
+        default:
+          return;
+      }
+    case "scissors":
+      switch (computerSelection) {
+        case "scissors":
+          return "Tie! Go again!!";
+          break;
+        case "rock":
+          deductPoint(playerScore);
+          computerScore++;
+          return "You lose! Rock crushes paper!!";
+          break;
+        case "paper":
+          deductPoint(computerScore);
+          playerScore++;
+          return "You win! Scissors cut paper!";
+          break;
+        default:
+          return;
+      }
+  }
+};
+
+// Start a game of "Rock, Paper, Scissors"
+function startGame() {
+  // clear the console
+  console.clear();
+
+  // loop through 5 rounds of rock, paper, scissors
+  for (let i = 0; i < 5; i++) {
+    // get players' answers
+    let playerAnswer = prompt("Choose Rock, Paper, or Scissors");
+    let computerAnswer = computerPlay();
+
+    // play round with players' answers
+    playRound(playerAnswer.toLowerCase(), computerAnswer.toLowerCase());
+
+    // display current game stats (in console)
+    console.log(
+      "Game " + (i + 1) + ": Player: ",
+      playerAnswer,
+      "Computer: ",
+      computerAnswer
+    );
+  }
+
+  // display player and computer score in console
+  displayScore(playerScore, computerScore);
+}
